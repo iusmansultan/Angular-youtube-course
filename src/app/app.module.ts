45d4bda41527ef4e505eddcb11dff15d6f4b953f
Muhammad Usman <iusmansultan@gmail.com>
2023-03-11 00:51:16 +0500

    sign in done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './pages/signin/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
